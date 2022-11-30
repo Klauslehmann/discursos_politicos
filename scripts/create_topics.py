8c136486cada22bba6f6fd1cd3e2f23b7a22611f
Klaus <klehmann@fen.uchile.cl>
2022-11-30 14:04:36 -0300

    ajustar script topicos

--- scripts/create_topics.py
+++ scripts/create_topics.py
@@ -9,10 +9,8 @@ import time
 # CREAR TÓPICOS 
 ###############
 
-#df_full = pd.read_feather("data/score_full.feather")
-#df_full .to_csv("data/score_full.csv")
+df_full = pd.read_feather("data/score_full.feather")
 
-df_full = pd.read_csv("data/score_full.csv")
 df_filtrado = df_full[df_full["n_words"] >= 3 ].reset_index()
 
 # Cargar el modelo para encontrar tópicos
@@ -28,7 +26,7 @@ topics = ["salud", "educación", "deporte",
 
 # Seleccionar una muestra para crear los tópicos
 n = df_filtrado.shape[0]
-n = 10
+n = 1200000
 tx  = df_filtrado.text.sample(n = n, random_state = 1,)
 
 # Clasificar cada texto con un tópico
@@ -44,28 +42,44 @@ labels = [t["labels"][0] for t in topic]
 score_topic = [t["scores"][0] for t in topic]
 indices = tx.index
 
-# Constuir dataframe con un subconjunto de textos clasificados con tópico 
-dic = {"id_phrase": df_filtrado.id_phrase[indices],
-       "id_original_text": df_filtrado.id_speech[indices],
-       "fecha": df_filtrado.fecha[indices],
-       "name": df_filtrado.nombre[indices],
-       "text": tx, 
-       "topic": labels,
-       "score_topic": score_topic,
-       "score":df_filtrado.score[indices],
-       "pole": df_filtrado.polo[indices],
-       "emotion": df_filtrado.cos_affect[indices], 
-       "cognition": df_filtrado.cos_cognitive[indices],
-       "age": df_filtrado.edad_actual[indices],
-       "year": df_filtrado.anio[indices],
-       "sex": df_filtrado.sexo[indices] 
-       }
- 
-df_topics= pd.DataFrame(dic ).reset_index()
+
+# Construir una tabla que contiene el identificador del texto junto a su tópico
+topics_dic = {
+    "id_phrase": df_filtrado.id_phrase[indices],
+    "topic": labels
+    }
+
+df_topics= pd.DataFrame(topics_dic ).reset_index()
+
+
+# =============================================================================
+# 
+# # Constuir dataframe con un subconjunto de textos clasificados con tópico 
+# dic = {"id_phrase": df_filtrado.id_phrase[indices],
+#        "id_original_text": df_filtrado.id_speech[indices],
+#        "fecha": df_filtrado.fecha[indices],
+#        "name": df_filtrado.nombre[indices],
+#        "text": tx, 
+#        "topic": labels,
+#        "score_topic": score_topic,
+#        "score":df_filtrado.score[indices],
+#        "pole": df_filtrado.polo[indices],
+#        "emotion": df_filtrado.cos_affect[indices], 
+#        "cognition": df_filtrado.cos_cognitive[indices],
+#        "age": df_filtrado.edad_actual[indices],
+#        "year": df_filtrado.anio[indices],
+#        "sex": df_filtrado.sexo[indices] 
+#        }
+#  
+# df_topics= pd.DataFrame(dic ).reset_index()
+# =============================================================================
+
+
+
+
 
 #################
 # GUARDAR DATOS #
 #################
-df_filtrado .to_feather("data/score_filtered.feather")
-df_filtrado .to_csv("data/score_filtered.csv")
+df_topics.to_feather("data/predicted_topics.feather")
 
